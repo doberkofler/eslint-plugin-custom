c85667435c71241ce16d61bf5fd857d6ae8b52fa
Dieter Oberkofler <dieter.oberkofler@gmail.com>
2019-04-04 14:40:04 +0200

    finalized

--- rules/force-function-argument-names.js
+++ rules/force-function-argument-names.js
@@ -9,13 +9,39 @@
 // Helpers
 //------------------------------------------------------------------------------
 
-const functionNames = ['jsonGet', 'jsonGetSuccess', 'jsonPost', 'jsonPostSuccess'];
+function isValid(restricted, expression) {
+	// if this is not a function call, we are done
+	if (expression.type !== "CallExpression") {
+		return true;
+	}
+
+	// if the name of the function does not match, we are done
+	if (!restricted.hasOwnProperty(expression.callee.name)) {
+		return true;
+	}
+	const option = restricted[expression.callee.name];
+
+	return expression.arguments.every((argument, index) => {
+		// Is this argument an "Identifier"
+		if (argument.type !== 'Identifier') {
+			return true;
+		}
+
+		// Has this parameter been specified in the options
+		if (!option.hasOwnProperty(index)) {
+			return true;
+		}
+
+		return option[index] === expression.arguments[index].name;
+	});
+}
 
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
 
 module.exports = {
+
 	meta: {
 		type: "suggestion",
 		docs: {
@@ -24,17 +50,56 @@ module.exports = {
 			recommended: false,
 			//url: "https://eslint.org/docs/rules/camelcase"
 		},
-		schema: [] // no options
+		schema: {
+			type: "array",
+			items: {
+				type: "object",
+				properties: {
+					name: {type: "string"},
+					args: {
+						type: "array",
+						items: {
+							type: "object",
+							properties: {
+								index: {type: "number"},
+								name: {type: "string"}
+							},
+							required: ["index", "name"],
+							additionalProperties: false
+						}
+					}
+				},
+				required: ["name", "args"],
+				additionalProperties: false
+			}
+		},
+		uniqueItems: true,
+		minItems: 0
 	},
+
 	create(context) {
+
+		// If no globals are restricted, we don't need to do anything
+		if (context.options.length === 0) {
+			return {};
+		}
+
+		// optimize the option for later processing
+		const restricted = context.options.reduce((prev, option) => {
+			if (option.args.length > 0) {
+				prev[option.name] = option.args.reduce((prev, curr) => {
+					prev[curr.index] = curr.name;
+					return prev;
+				}, {});
+			}
+			return prev;
+		}, {}); 
+
 		return {
 			ExpressionStatement(node) {
 				const expression = node.expression;
 
-				/*
-				 * Invoking a function named "jsonGet" with 2 arguments and the second argument does not have the name "para"
-				 */
-				if (expression.type === "CallExpression" && functionNames.indexOf(expression.callee.name) !== -1 && expression.arguments.length === 2 && expression.arguments[1].name !== 'para') {
+				if (!isValid(restricted, node.expression)) {
 					context.report({
 						node,
 						message: 'The second argument in the function "' + expression.callee.name + '" must always be named "para"'
--- tests/force-function-argument-names.js
+++ tests/force-function-argument-names.js
@@ -3,39 +3,171 @@
 const rule = require('../rules/force-function-argument-names')
 const RuleTester = require('eslint').RuleTester
 const ruleTester = new RuleTester({
-  parserOptions: {
-	ecmaVersion: 6
-  }
+	parserOptions: {
+		ecmaVersion: 6
+	}
 })
 
-ruleTester.run('force-function-argument-names', rule, {
-  valid: [
-	'jsonGet("url");',
-	'jsonGet("url", para);',
-	'jsonPost("url");',
-	'jsonPost("url", para);',
-	'jsonGetSuccess("url");',
-	'jsonGetSuccess("url", para);',
-	'jsonPostSuccess("url");',
-	'jsonPostSuccess("url", para);',
-  ],
+const defaultOptions = [
+	{
+		name: "jsonGet",
+		args: [{index: 1, name: "para"}]
+	},
+	{
+		name: "jsonGetSuccess",
+		args: [{index: 1, name: "para"}]
+	},
+	{
+		name: "jsonGetValidate",
+		args: [{index: 1, name: "para"}]
+	},
+	{
+		name: "jsonPost",
+		args: [{index: 1, name: "para"}]
+	},
+	{
+		name: "jsonPostSuccess",
+		args: [{index: 1, name: "para"}]
+	},
+	{
+		name: "jsonPostValidate",
+		args: [{index: 1, name: "para"}]
+	},
+	{
+		name: "jsonPostMessage",
+		args: [{index: 1, name: "para"}]
+	},
+];
 
-  invalid: [
-	{
-		code: 'jsonGet("url", data);',
-		errors: [{ message: 'The second argument in the function "jsonGet" must always be named "para"' }]
-	},
-	{
-		code: 'jsonPost("url", data);',
-		errors: [{ message: 'The second argument in the function "jsonPost" must always be named "para"' }]
-	},
-	{
-		code: 'jsonGetSuccess("url", data);',
-		errors: [{ message: 'The second argument in the function "jsonGetSuccess" must always be named "para"' }]
-	},
-	{
-		code: 'jsonPostSuccess("url", data);',
-		errors: [{ message: 'The second argument in the function "jsonPostSuccess" must always be named "para"' }]
-	},
+ruleTester.run('force-function-argument-names', rule, {
+	valid: [
+		{
+			code: 'some("1");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'some("1", "2");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'some("1", "2", "3");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGet("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGet("url", {});',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGet("url", para);',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGetSuccess("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGetSuccess("url", para);',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGetValidate("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonGetValidate("url", para);',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPost("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPost("url", para);',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPostSuccess("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPostSuccess("url", para);',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPostValidate("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPostValidate("url", para);',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPostMessage("url");',
+			options: defaultOptions,
+
+		},
+		{
+			code: 'jsonPostMessage("url", para);',
+			options: defaultOptions,
+
+		},
+	],
+
+	invalid: [
+		{
+			code: 'jsonGet("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonGet" must always be named "para"'}]
+		},
+		{
+			code: 'jsonGetSuccess("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonGetSuccess" must always be named "para"'}]
+		},
+		{
+			code: 'jsonGetValidate("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonGetValidate" must always be named "para"'}]
+		},
+		{
+			code: 'jsonPost("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonPost" must always be named "para"'}]
+		},
+		{
+			code: 'jsonPostSuccess("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonPostSuccess" must always be named "para"'}]
+		},
+		{
+			code: 'jsonPostValidate("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonPostValidate" must always be named "para"'}]
+		},
+		{
+			code: 'jsonPostMessage("url", data);',
+			options: defaultOptions,
+			errors: [{message: 'The second argument in the function "jsonPostMessage" must always be named "para"'}]
+		},
 	]
 })
